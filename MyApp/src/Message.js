0ce9f6244ac674dbc0553fbe5f5617bd737c692b
Full-Hole <76530118+Full-Hole@users.noreply.github.com>
2022-04-06 10:44:40 +0300

    merge hw2 in myapp (#5)
    Co-authored-by: Timur G <gavryushov.t@medindex.ru>

--- MyApp/src/Message.js
+++ MyApp/src/Message.js
@@ -1,5 +1,10 @@
-function Message(props){
-    return <p className="Message-text">{props.text}</p>
+const Message =({data})=>{
+    return (
+    <div className={data.author === "Bot" ? 'message-wrapper bot-message' : "message-wrapper"}>
+        <div className="message-author">{data.author}</div>
+        <div className="message-text">{data.text}</div>
+    </div>
+    )
 }
 
 export default Message;
