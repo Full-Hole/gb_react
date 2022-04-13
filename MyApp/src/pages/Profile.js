8094862ecf9a6ee5707196062b60879adfa51e8a
Timur G <gavryushov.t@medindex.ru>
2022-04-13 17:55:08 +0300

    add username to store

--- MyApp/src/pages/Profile.js
+++ MyApp/src/pages/Profile.js
@@ -1,27 +1,29 @@
 import { useCallback, useState} from 'react';
 import { Button, TextField } from '@mui/material';
-import { changeVisible, changeCheckbox } from '../store/profile/actions';
+import { changeVisible, updateName} from '../store/profile/actions';
 import { useDispatch, useSelector } from 'react-redux';
 const Profile = () => {
-    const [userName, setUserName] = useState('');
-    const { showName, name, checkboxState } = useSelector((state) => state)
+    
+    const { showName, name } = useSelector((state) => state)
     const dispatch = useDispatch();
+    const [userName, setUserName] = useState(name);
     //const [, setDummy] = useState();
 
     const setShowName = useCallback(() => {
         dispatch(changeVisible);
     }, [dispatch]);
 
-    const setCheckbox = useCallback(() => {
-        dispatch(changeCheckbox);
-    }, [dispatch]);
+    const saveName = useCallback(() => {    
+        dispatch(updateName(userName));
+    }, [dispatch, userName]);
 
-    const saveName =() => {
-        console.log('nen');
-    }
+    // const saveName =() => {
+    //     console.log('nen');
+    // }
 
     const handleNameInput = (e) => {
-        setUserName(userName);
+        setUserName(e.target.value);
+       
     }
 
 
@@ -29,9 +31,7 @@ const Profile = () => {
         <h1>Profile</h1>
         <button onClick={setShowName}>Show Name</button>
         <blockquote>{showName && <h3>{name}</h3>}</blockquote>
-        <p>Checkbox</p>
-        
-        <input type='checkbox' checked={checkboxState} onChange={setCheckbox} />
+       
         <br/>
         <TextField id="outlined-basic" label="Имя" variant="outlined" onChange={handleNameInput}
                 value={userName}/>
