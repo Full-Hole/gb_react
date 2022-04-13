3119946b070cf7ddc925e46eedc6a51ff6ac42e0
Timur G <gavryushov.t@medindex.ru>
2022-04-13 16:51:26 +0300

    add name field in profile

--- MyApp/src/pages/Profile.js
+++ MyApp/src/pages/Profile.js
@@ -1,20 +1,28 @@
-import { useCallback, } from 'react';
-
+import { useCallback, useState} from 'react';
+import { Button, TextField } from '@mui/material';
 import { changeVisible, changeCheckbox } from '../store/profile/actions';
 import { useDispatch, useSelector } from 'react-redux';
 const Profile = () => {
+    const [userName, setUserName] = useState('');
     const { showName, name, checkboxState } = useSelector((state) => state)
     const dispatch = useDispatch();
     //const [, setDummy] = useState();
 
     const setShowName = useCallback(() => {
         dispatch(changeVisible);
-        //setDummy({});
     }, [dispatch]);
+
     const setCheckbox = useCallback(() => {
         dispatch(changeCheckbox);
-        //setDummy({});
     }, [dispatch]);
+
+    const saveName =() => {
+        console.log('nen');
+    }
+
+    const handleNameInput = (e) => {
+        setUserName(userName);
+    }
 
 
     return (<div>
@@ -22,7 +30,15 @@ const Profile = () => {
         <button onClick={setShowName}>Show Name</button>
         <blockquote>{showName && <h3>{name}</h3>}</blockquote>
         <p>Checkbox</p>
+        
         <input type='checkbox' checked={checkboxState} onChange={setCheckbox} />
+        <br/>
+        <TextField id="outlined-basic" label="Имя" variant="outlined" onChange={handleNameInput}
+                value={userName}/>
+                <br/>
+        <Button variant="contained" onClick={saveName}>
+                Сохранить
+            </Button>
     </div>
     )
 };
